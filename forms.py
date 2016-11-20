0aaa1678044941eaa4d4da4156b81797d7e4f6a1
Andre Miras <andre.miras@gmail.com>
2016-11-20 13:38:23 +0100

    Update forms.py, voice is optional
    Defaults to en-GB.

--- forms.py
+++ forms.py
@@ -2,13 +2,18 @@ from wtforms import Form, validators, TextAreaField, SelectField, FloatField
 from libnanotts import NanoTts
 
 
+VOICE_CHOICES = [(voice, voice) for voice in NanoTts.get_voices()]
+
+
 class NanoTtsForm(Form):
     text = TextAreaField(
         u'Text',
         [validators.InputRequired(), validators.length(max=200)],
         default=u"Nano T T S service.")
     voice = SelectField(
-        choices=[(voice, voice) for voice in NanoTts.get_voices()])
+                validators=[validators.Optional()],
+                choices=VOICE_CHOICES,
+                default='en-GB')
     speed = FloatField(validators=[
                 validators.Optional(), validators.NumberRange(0.2, 5.0)])
     pitch = FloatField(validators=[
