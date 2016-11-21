3a4562db1d16061f46b972a93869e6a4909a5ae4
Andre Miras <andre.miras@gmail.com>
2016-11-21 21:09:49 +0100

    Update webapp_tests.py, implement test_api()

--- webapp_tests.py
+++ webapp_tests.py
@@ -1,11 +1,14 @@
-import unittest
+import json
 import webapp
+import unittest
 
 
 class WebappTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.app = webapp.app.test_client()
+        self.app = webapp.app
+        self.app.config['WTF_CSRF_ENABLED'] = False
+        self.client = webapp.app.test_client()
 
     def tearDown(self):
         pass
@@ -15,8 +18,32 @@ class WebappTestCase(unittest.TestCase):
         Very simple home test case.
         Simply verifies the status code is OK.
         """
-        response = self.app.get('/')
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_api(self):
+        """
+        Simple API test cases.
+        1) minimal test case, just sending a text to the API
+        2) asking for audio file address rather than content
+        """
+        api_url = '/api'
+        # 1) minimal test case, just sending a text to the API
+        data = {"text": "Test text"}
+        response = self.client.post(api_url, data=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content_type, "audio/x-wav")
+        # 2) asking for audio file address rather than content
+        data = {
+            "text": "Test text",
+            "response_type": "audio_address",
+        }
+        response = self.client.post(api_url, data=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content_type, "application/json")
+        json_response = json.loads(response.get_data(as_text=True))
+        # they audio_file key, contains the file address
+        self.assertTrue('audio_file' in json_response)
 
 
 if __name__ == '__main__':
